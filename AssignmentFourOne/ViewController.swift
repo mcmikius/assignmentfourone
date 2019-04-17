d83020bc7ff74c6e602dfa05048e54a03ccf30b8
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-17 09:34:34 +0300

    create drawSquare func

--- AssignmentFourOne/ViewController.swift
+++ AssignmentFourOne/ViewController.swift
@@ -15,8 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        drawSquare()
+    }
+
+    func drawSquare() {
+        let square = UIView()
+        square.frame.size.width = 50
+        square.frame.size.height = 50
+        square.frame.origin.x = 20
+        square.frame.origin.y = 20
+        square.backgroundColor = .gray
+        view.addSubview(square)
+    }
+
+
+
+
 
 }
 
