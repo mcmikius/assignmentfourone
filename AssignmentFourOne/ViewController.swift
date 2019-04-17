5fe17ba55b269df25ace5f3be0140fcffc7bbfa4
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-17 09:53:19 +0300

    create drawLine func

--- AssignmentFourOne/ViewController.swift
+++ AssignmentFourOne/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let numberOfCubesPerLine = 3
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,16 +20,26 @@ class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         drawSquare()
+        drawLine(number: numberOfCubesPerLine, startX: <#T##Double##Swift.Double#>, StartY: <#T##Double##Swift.Double#>)
     }
 
     func drawSquare() {
         let square = UIView()
         square.frame.size.width = 50
         square.frame.size.height = 50
-        square.frame.origin.x = 20
-        square.frame.origin.y = 20
         square.backgroundColor = .gray
         view.addSubview(square)
+    }
+
+    func drawOneSquare() {
+        drawSquare()
+    }
+
+    func drawLine(number: Int, startX: Double, StartY: Double) {
+        for i in 0..<number {
+            drawSquare()
+        }
+
     }
 
 
